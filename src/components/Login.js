0c760f3355ace7f652e44a09edf765bc8aca9dc7
brandon <srlguitarist@gmail.com>
2022-08-25 23:52:08 -0400

    Move file

---
+++ src/components/Login.js
@@ -0,0 +1,35 @@
+import { Link } from "react-router-dom";
+
+export default function Login({
+  onSubmit,
+  loginEmail,
+  setLoginEmail,
+  loginPassword,
+  setLoginPassword,
+}) {
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={onSubmit}>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="text"
+          name="email"
+          value={loginEmail}
+          onChange={(e) => setLoginEmail(e.target.value)}
+        />
+        <br />
+        <br />
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          name="password"
+          value={loginPassword}
+          onChange={(e) => setLoginPassword(e.target.value)}
+        />
+        <input type="submit" value="Login" />
+      </form>
+      <Link to="/register">Register</Link>
+    </div>
+  );
+}
