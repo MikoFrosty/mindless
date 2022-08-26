36fb9dffb7121ed4dc63d06d8c0980ffb7348f67
brandon <srlguitarist@gmail.com>
2022-08-25 23:52:36 -0400

    Move file

---
+++ src/components/Register.js
@@ -0,0 +1,35 @@
+import { Link } from "react-router-dom";
+
+export default function Register({
+  onSubmit,
+  registerEmail,
+  setRegisterEmail,
+  registerPassword,
+  setRegisterPassword,
+}) {
+  return (
+    <div>
+      <h1>Register</h1>
+      <form onSubmit={onSubmit}>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="text"
+          name="email"
+          value={registerEmail}
+          onChange={(e) => setRegisterEmail(e.target.value)}
+        />
+        <br />
+        <br />
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          name="password"
+          value={registerPassword}
+          onChange={(e) => setRegisterPassword(e.target.value)}
+        />
+        <input type="submit" value="Register" />
+      </form>
+      <Link to="/login">Login</Link>
+    </div>
+  );
+}
