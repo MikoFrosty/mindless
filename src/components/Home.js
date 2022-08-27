adb8231fc07a9e3010722f81c251175cff262744
brandon <srlguitarist@gmail.com>
2022-08-26 22:01:20 -0400

    Add button, basic outline for further work

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,10 +4,12 @@ import Header from "./Header";
 export default function Home({ onLogoutClick, user }) {
   return (
     <div>
-      <Header pageName="Home" showProfileBtn={true} user={user} showBackBtn={false}/>
-      <p>Welcome, {user.email}</p>
-      <Link to="/home/tasklist">Task List</Link>
-      <input type="button" value="Logout" onClick={onLogoutClick} />
+      <Header pageName="Home" hideProfileBtn={false} user={user} hideBackBtn={true}/>
+      <br />
+      <button onClick={() => {}}>Start Routine</button>
+      <br />
+      <br />
+      <Link to="/home/tasklist">Task List</Link> 
     </div>
   );
 }
