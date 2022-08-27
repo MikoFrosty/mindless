cb1f4050391204f0ed0a052784857d1deaf889ef
brandon <srlguitarist@gmail.com>
2022-08-26 22:00:37 -0400

    Change show to hide on prop names

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,20 +3,20 @@ import "./Header.css";
 import { useNavigate } from "react-router-dom";
 
 export default function Header({
-  showBackBtn,
+  hideBackBtn,
   pageName,
-  showProfileBtn,
+  hideProfileBtn,
   onLogoutClick,
   user,
 }) {
   const navigate = useNavigate();
   return (
     <div id="header">
-      <button className={showBackBtn || "hide"} onClick={() => navigate(-1)}>
+      <button className={hideBackBtn ? "hide" : undefined} onClick={() => navigate(-1)}>
         Back
       </button>
       <h1>{pageName}</h1>
-      <Link to="/home/profile" className={showProfileBtn || "hide"}>
+      <Link to="/home/profile" className={hideProfileBtn ? "hide" : undefined}>
         Profile
       </Link>
     </div>
