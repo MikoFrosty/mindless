f48dfa233194d7f7bb88b9b005e0e66fb2fa37b8
brandon <srlguitarist@gmail.com>
2022-08-28 23:53:31 -0400

    Add line-awesome icons and props

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,24 @@
 import { Link } from "react-router-dom";
 import Header from "./Header";
+import "./Home.css";
 
-export default function Home({ onLogoutClick, user }) {
+export default function Home({ user }) {
   return (
-    <div>
-      <Header pageName="Home" hideProfileBtn={false} user={user} hideBackBtn={true}/>
-      <br />
-      <button onClick={() => {}}>Start Routine</button>
-      <br />
-      <br />
-      <Link to="/home/tasklist">Task List</Link> 
-    </div>
+    <>
+      <Header
+        pageName="Home"
+        hideProfileBtn={false}
+        user={user}
+        hideBackBtn={true}
+      />
+      <div id="home-page">
+        <button id="start-button" onClick={() => {}}>
+        Start Routine
+        </button>
+        <Link id="tasklist-button" to="/home/tasklist">
+        <i class="las la-edit"></i> EDIT/VIEW TASKS
+        </Link>
+      </div>
+    </>
   );
 }
