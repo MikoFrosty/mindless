ebd656db5e9e1211b85615e134ad50e08ca3ac2f
brandon <srlguitarist@gmail.com>
2022-08-28 23:52:36 -0400

    Move header outside div

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -3,11 +3,14 @@ import Task from "./Task";
 
 export default function TaskList() {
   return (
-    <div>
-      <Header pageName="Task List" hideProfileBtn={true} hideBackBtn={false}/>
-      <button onClick={() => {}}>Add Task</button>
-      <p>Task List</p>
-      <Task />
-    </div>
+    <>
+      <Header pageName="Task List" hideProfileBtn={true} hideBackBtn={false} />
+      <div>
+        <button onClick={() => {}}>Add Task</button>
+        <p>Task List</p>
+        {/* <Task /> */}
+        <Task />
+      </div>
+    </>
   );
 }
