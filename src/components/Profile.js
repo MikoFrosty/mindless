620f34f48f3d088a316100f13ea7a8955a9d6a56
brandon <srlguitarist@gmail.com>
2022-08-28 23:54:01 -0400

    Move header outside div, add props

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,12 +1,15 @@
 import Header from "./Header";
+import "./Profile.css";
 
 export default function Profile({ user, logout }) {
   return (
-    <div>
+    <>
       <Header pageName="Profile" hideProfileBtn={true} hideBackBtn={false} />
-      <p>You are: {user.email}</p>
-      <p>FORM AREA HERE</p>
-      <button onClick={logout}>Logout</button>
-    </div>
+      <div id="profile-page">
+        <p>You are: {user.email}</p>
+        <p>FORM AREA HERE</p>
+        <button onClick={logout}>Logout</button>
+      </div>
+    </>
   );
 }
