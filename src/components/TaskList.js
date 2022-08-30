59f9f0b8397ab1d36d8a657511ba21ffb4889ced
brandon <srlguitarist@gmail.com>
2022-08-30 00:23:03 -0400

    Overhaul page -- Items added: css file import, states & effects for add task form, functionality for adding new tasks via form component, display tasklist using component with props.

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,15 +1,59 @@
 import Header from "./Header";
+import AddTaskForm from "./AddTaskForm";
 import Task from "./Task";
+import { useState, useEffect } from "react";
+import "./TaskList.css";
 
-export default function TaskList() {
+export default function TaskList({ taskList, setTaskList }) {
+  const [showAddTaskForm, setShowAddTaskForm] = useState(false);
+  const [newTaskName, setNewTaskName] = useState("");
+  const [newTaskOrder, setNewTaskOrder] = useState(0);
+
+  const handleAddTaskClick = () => {
+    setShowAddTaskForm(true);
+  };
+
+  const handleAddTaskFormSubmit = (e) => {
+    e.preventDefault();
+    setTaskList(() => [
+      ...taskList,
+      {
+        name: newTaskName,
+        order: newTaskOrder,
+      },
+    ]);
+    setNewTaskName("");
+    setNewTaskOrder(0);
+    setShowAddTaskForm(false);
+  };
+
+  useEffect(() => {
+    localStorage.setItem("taskList", JSON.stringify(taskList));
+  }, [taskList]);
+
   return (
     <>
       <Header pageName="Task List" hideProfileBtn={true} hideBackBtn={false} />
-      <div>
-        <button onClick={() => {}}>Add Task</button>
-        <p>Task List</p>
-        {/* <Task /> */}
-        <Task />
+      <div id="tasklist-page">
+        {showAddTaskForm ? (
+          <AddTaskForm
+            setShowAddTaskForm={setShowAddTaskForm}
+            handleAddTaskFormSubmit={handleAddTaskFormSubmit}
+            newTaskName={newTaskName}
+            setNewTaskName={setNewTaskName}
+            newTaskOrder={newTaskOrder}
+            setNewTaskOrder={setNewTaskOrder}
+          />
+        ) : null}
+        <button id="add-task-button" onClick={handleAddTaskClick}>
+          <i class="las la-plus-circle"></i> Add Task
+        </button>
+        <h3 id="task-list-label">Task List</h3>
+        {taskList.length
+          ? taskList
+              .sort((a, b) => a.order - b.order)
+              .map((task, index) => <Task key={index} task={task} />)
+          : "No tasks"}
       </div>
     </>
   );
