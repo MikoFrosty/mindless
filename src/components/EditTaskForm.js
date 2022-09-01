be74d20fa81f187a0733db412e6f1b72ec95d919
brandon <srlguitarist@gmail.com>
2022-09-01 13:07:07 -0400

    Create file, add basic functionality

---
+++ src/components/EditTaskForm.js
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import "./EditTaskForm.css";
+
+export default function EditTaskForm({
+  setShowEditTaskForm,
+  handleEditTaskFormSubmit,
+  editTaskName,
+  setEditTaskName,
+  editTaskOrder,
+  setEditTaskOrder,
+}) {
+  const [originalTaskName, setOriginalTaskName] = useState(editTaskName);
+  const [originalTaskOrder, setOriginalTaskOrder] = useState(editTaskOrder);
+
+  return (
+    <div id="edit-task-form-div">
+      <form
+        id="edit-task-form"
+        onSubmit={(event) =>
+          handleEditTaskFormSubmit(event, originalTaskName, originalTaskOrder)
+        }
+      >
+        <label htmlFor="task-name">Task Name:</label>
+        <input
+          type="text"
+          name="task-name"
+          value={editTaskName}
+          onChange={(e) => setEditTaskName(e.target.value)}
+        />
+        <label htmlFor="task-order">Task Order:</label>
+        <input
+          value={editTaskOrder}
+          onChange={(e) => setEditTaskOrder(e.target.value)}
+          type="number"
+          name="task-order"
+          min="0"
+          max="1000"
+          inputMode="numeric"
+          pattern="[0-9]*"
+        />
+        <input type="submit" id="edit-task-button" value="Edit Task" />
+      </form>
+      <button
+        id="cancel-edit-task-button"
+        onClick={() => setShowEditTaskForm(false)}
+      >
+        Cancel
+      </button>
+    </div>
+  );
+}
