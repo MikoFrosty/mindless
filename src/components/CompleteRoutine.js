cdf1d4e519105a93ca62b1cd42b2e7ecdfe99067
brandon <srlguitarist@gmail.com>
2022-09-02 13:42:07 -0400

    Create file

---
+++ src/components/CompleteRoutine.js
@@ -0,0 +1,9 @@
+import "./CompleteRoutine.css";
+
+export default function CompleteRoutine() {
+  return (
+    <div id="complete-routine-page">
+      <h1>Complete Routine</h1>
+    </div>
+  );
+}
