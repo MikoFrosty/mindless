8b07d62f9dfa6f12c78073b3bfcd47c711fb204c
brandon <srlguitarist@gmail.com>
2022-09-02 13:41:40 -0400

    Add number parsing for onchange, edit prop names

--- src/components/AddTaskForm.js
+++ src/components/AddTaskForm.js
@@ -2,7 +2,7 @@ import "./AddTaskForm.css";
 
 export default function AddTaskForm({
   setShowAddTaskForm,
-  handleAddTaskFormSubmit,
+  onAddTaskFormSubmit,
   newTaskName,
   setNewTaskName,
   newTaskOrder,
@@ -10,13 +10,18 @@ export default function AddTaskForm({
 }) {
   return (
     <div id="add-task-form-div">
-      <form id="add-task-form" onSubmit={handleAddTaskFormSubmit}>
+      <form id="add-task-form" onSubmit={onAddTaskFormSubmit}>
         <label htmlFor="task-name">Task Name:</label>
-        <input type="text" name="task-name" value={newTaskName} onChange={(e) => setNewTaskName(e.target.value)}/>
+        <input
+          type="text"
+          name="task-name"
+          value={newTaskName}
+          onChange={(e) => setNewTaskName(e.target.value)}
+        />
         <label htmlFor="task-order">Task Order:</label>
         <input
           value={newTaskOrder}
-          onChange={(e) => setNewTaskOrder(e.target.value)}
+          onChange={(e) => setNewTaskOrder(Number.parseInt(e.target.value, 10))}
           type="number"
           name="task-order"
           min="0"
