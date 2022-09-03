53ddedb7fdb4287e519a90fc53002ca225859eb8
brandon <srlguitarist@gmail.com>
2022-09-03 13:46:46 -0400

    Add taskDrop code, add data transfer. Remove some unused code

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,37 +1,39 @@
 import "./Task.css";
 
-export default function Task({ task, onTaskDelete, onEditTaskClick }) {
+export default function Task({
+  task,
+  onTaskDelete,
+  onEditTaskClick,
+  onTaskDrop,
+}) {
   const { name, order } = task;
 
   const handleTaskDragStart = (e) => {
+    e.dataTransfer.setData("order", order);
     e.target.style.opacity = "0.4";
-    // stop propagation of the event to prevent the browser from dragging the element
     e.stopPropagation();
   };
   const handleTaskDragEnd = (e) => {
     e.target.style.opacity = "1";
     e.target.classList.remove("over");
   };
-
+  const handleTaskDragEnter = (e) => {
+    e.target.classList.add("over");
+  };
   const handleTaskDragOver = (e) => {
     e.preventDefault();
   };
-
-  const handleTaskDragEnter = (e) => {
-    //e.target.style.backgroundColor = "lightblue";
-    e.target.classList.add("over");
-  };
   const handleTaskDragLeave = (e) => {
     e.target.classList.remove("over");
-    //e.target.style.backgroundColor = "white";
   };
-
   const handleTaskDrop = (e) => {
     e.preventDefault();
+    let orderA = e.dataTransfer.getData("order");
+    if (orderA !== order) {
+      onTaskDrop(orderA, order);
+    }
     e.target.classList.remove("over");
     e.stopPropagation();
-    //const data = e.dataTransfer.getData("text");
-    //onTaskDelete(data);
   };
 
   return (
