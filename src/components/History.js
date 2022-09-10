bc06a36cda82337f0a49a0f42e7703b352e84c58
brandon <srlguitarist@gmail.com>
2022-09-10 18:15:43 -0400

    Complete History component

--- src/components/History.js
+++ src/components/History.js
@@ -1,9 +1,84 @@
+import { useEffect, useState } from "react";
+import timeDuration from "../utils/timeDuration";
+import Header from "./Header";
 import "./History.css";
 
-export default function History() {
-    return (
-        <div id="history-page">
-        <h2>History</h2>
+export default function History({ getHistory, onTimestampDelete }) {
+  const [history, setHistory] = useState({});
+  const [historyUpdated, setHistoryUpdated] = useState(0);
+
+  useEffect(() => {
+    async function getHistoryData() {
+      const data = await getHistory();
+      setHistory(() => data);
+    }
+    getHistoryData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [historyUpdated]);
+
+  const historyList = history?.timestamps
+    ?.map((timestamp, index, arr) => {
+      const startDate = new Date(timestamp.start).toLocaleDateString("en-US", {
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+      });
+      const startTime = new Date(timestamp.start).toLocaleString("en-US", {
+        hour: "numeric",
+        minute: "numeric",
+        hour12: true,
+        second: "numeric",
+      });
+      const endTime = new Date(timestamp.end).toLocaleString("en-US", {
+        hour: "numeric",
+        minute: "numeric",
+        hour12: true,
+        second: "numeric",
+      });
+      const duration = timeDuration(timestamp.start, timestamp.end);
+      return (
+        <tr key={index}>
+          <td>{startDate}</td>
+          <td>{startTime}</td>
+          <td>{endTime}</td>
+          <td>{duration}</td>
+          <td className="td-timestamp-delete">
+            <button
+              className="timestamp-delete-button"
+              onClick={() => {
+                onTimestampDelete(arr[index]).then(
+                  setHistoryUpdated((prev) => prev + 1)
+                );
+              }}
+            >
+              <i className="las la-trash"></i>
+            </button>
+          </td>
+        </tr>
+      );
+    })
+    .reverse();
+
+  return (
+    <>
+      <Header pageName="History" hideProfileBtn={true} hideBackBtn={false} />
+      <div id="history-page">
+        <h2>Run Times for Default Routine</h2>
+        <div id="table-container">
+          <table>
+            <thead>
+              <tr>
+                <th>Date (Top - Recent)</th>
+                <th>Start Time</th>
+                <th>End Time</th>
+                <th>Duration</th>
+              </tr>
+            </thead>
+            <tbody>{historyList}</tbody>
+          </table>
         </div>
-    );
+      </div>
+    </>
+  );
 }
