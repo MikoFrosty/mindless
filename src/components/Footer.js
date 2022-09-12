725c9a12fd91e8e339bac0b6ab6ddad02c4a7cb8
brandon <srlguitarist@gmail.com>
2022-09-11 20:25:47 -0400

    Finish basic footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <footer>
+      <p>
+        Created By:{" "}
+        <a href="mailto:BrandonJMikowski+MindLess@gmail.com">
+          Brandon Mikowski
+        </a>
+      </p>
+    </footer>
+  );
+}
