21099d11d513dc9436e660716094ff323c916f4e
brandon <srlguitarist@gmail.com>
2023-02-22 23:49:00 -0500

    Remove uneaded useEffect wrapper

--- src/utils/ProtectedRoute.js
+++ src/utils/ProtectedRoute.js
@@ -1,14 +1,11 @@
 import { useNavigate } from "react-router-dom";
-import { useEffect } from "react";
 
 export default function ProtectedRoute({ children, user }) {
   const navigate = useNavigate();
-  useEffect(() => {
-    if (!user) {
-      navigate("/login", { replace: true });
-    }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+
+  if (!user) {
+    navigate("/login", { replace: true });
+  }
 
   return user ? children : null;
 }
